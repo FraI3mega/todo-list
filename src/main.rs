087680c794a6c17f2439fd456acbe5c64a82d068
FraI3mega <projeapp2@gmail.com>
2024-01-15 14:14:40 +0100

    refactor(task): move tasks to tasks.rs
    BREAKING CHANGE:

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
-use clap::{Parser, ValueEnum};
-use colored::Colorize;
+mod tasks;
 
-#[derive(Debug, PartialEq)]
-struct Task {
-    name: String,
-    done: bool,
-}
+use clap::{Parser, ValueEnum};
+use tasks::{add_task, print_tl, remove_task, Task};
 
 #[derive(Parser)]
 struct Cli {
@@ -51,84 +47,4 @@ fn main() {
         None => print_tl(&tasks),
     }
     print_tl(&tasks);
-}
-
-/// add a to to the tasklist
-fn add_task(list: &mut Vec<Task>, task: Task) {
-    list.push(task);
-}
-
-/// Remove item from tasklist. Takes in an index starting at 0
-fn remove_task(list: &mut Vec<Task>, index: usize) {
-    list.remove(index);
-}
-
-/// pretty prints the task list
-fn print_tl(list: &Vec<Task>) {
-    for task in list {
-        if &task.done == &false {
-            println!(
-                "{}. {}",
-                list.iter().position(|x| &x == &task).unwrap() + 1,
-                &task.name.blue()
-            )
-        } else {
-            println!(
-                "{}. {}",
-                list.iter().position(|x| &x == &task).unwrap() + 1,
-                &task.name.as_str().bright_black().strikethrough()
-            )
-        }
-    }
-}
-
-impl Task {
-    ///creates a undone task
-    pub fn create(name: String) -> Self {
-        Task { name, done: false }
-    }
-
-    ///marks task done
-    pub fn mark_done(&mut self) {
-        self.done = true;
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::{add_task, remove_task, Task};
-
-    #[test]
-    fn atask() {
-        assert_eq!(
-            Task::create("Test".to_string()),
-            Task {
-                name: "Test".to_string(),
-                done: false
-            }
-        );
-    }
-
-    #[test]
-    fn done() {
-        let mut t = Task::create("test".to_string());
-        t.mark_done();
-        assert_eq!(
-            t,
-            Task {
-                name: "test".to_string(),
-                done: true
-            }
-        );
-    }
-
-    #[test]
-    fn tl() {
-        let mut list: Vec<Task> = vec![];
-        assert!(list.is_empty());
-        add_task(&mut list, Task::create("test".to_string()));
-        assert_eq!(list, vec![Task::create("test".to_string())]);
-        remove_task(&mut list, 0);
-        assert!(list.is_empty());
-    }
 }
---
+++ src/tasks.rs
@@ -0,0 +1,87 @@
+use colored::Colorize;
+
+#[derive(Debug, PartialEq)]
+pub(crate) struct Task {
+    pub(crate) name: String,
+    pub(crate) done: bool,
+}
+
+/// add a to to the tasklist
+pub(crate) fn add_task(list: &mut Vec<Task>, task: Task) {
+    list.push(task);
+}
+
+/// Remove item from tasklist. Takes in an index starting at 0
+pub(crate) fn remove_task(list: &mut Vec<Task>, index: usize) {
+    list.remove(index);
+}
+
+/// pretty prints the task list
+pub(crate) fn print_tl(list: &Vec<Task>) {
+    for task in list {
+        if &task.done == &false {
+            println!(
+                "{}. {}",
+                list.iter().position(|x| &x == &task).unwrap() + 1,
+                &task.name.blue()
+            )
+        } else {
+            println!(
+                "{}. {}",
+                list.iter().position(|x| &x == &task).unwrap() + 1,
+                &task.name.as_str().bright_black().strikethrough()
+            )
+        }
+    }
+}
+
+impl Task {
+    ///creates a undone task
+    pub fn create(name: String) -> Self {
+        Task { name, done: false }
+    }
+
+    ///marks task done
+    pub fn mark_done(&mut self) {
+        self.done = true;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::tasks::{add_task, remove_task, Task};
+
+    #[test]
+    fn atask() {
+        assert_eq!(
+            Task::create("Test".to_string()),
+            Task {
+                name: "Test".to_string(),
+                done: false
+            }
+        );
+    }
+
+    #[test]
+    fn done() {
+        let mut t = Task::create("test".to_string());
+        t.mark_done();
+        assert_eq!(
+            t,
+            Task {
+                name: "test".to_string(),
+                done: true
+            }
+        );
+    }
+
+    #[test]
+    fn tl() {
+        let mut list: Vec<Task> = vec![];
+        assert!(list.is_empty());
+        add_task(&mut list, Task::create("test".to_string()));
+        assert_eq!(list, vec![Task::create("test".to_string())]);
+        remove_task(&mut list, 0);
+        assert!(list.is_empty());
+    }
+}
